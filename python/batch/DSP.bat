bc0aa9e0bfb325f97b758d487f70b8d860dacc8a
hesheng1996 <105448532+hesheng1996@users.noreply.github.com>
2024-01-26 11:04:38 +0800

    Add files via upload

---
+++ python/batch/DSP.bat
@@ -0,0 +1,4 @@
+@ ECHO OFF
+
+call C:\Users\459354\Downloads\"New Folder"\Scripts\activate.bat
+ipython C:\Users\459354\Desktop\Python\DSP_UTILISATION.ipynb
---
+++ python/batch/download_ongweishin.bat
@@ -0,0 +1,4 @@
+@ ECHO OFF
+
+call C:\Users\459354\Downloads\"New Folder"\Scripts\activate.bat
+ipython C:\Users\459354\Desktop\Python\download_attachment_ongweishin.ipynb
